701bf95e9327fc34e8c78bae7cca4bb666244fda
Alexey Bondarenko <alexeybondarenko@me.com>
2017-03-15 12:58:18 +0200

    Remove version

--- index.js
+++ index.js
@@ -7,15 +7,3 @@
 module.exports = require('./lib/sitemap');
 module.exports.utils = require('./lib/utils');
 module.exports.errors = require('./lib/errors');
-
-/**
- * Framework version.
- */
-var fs = require('fs')
-  , path = require('path')
-  , pack_file = path.join(__dirname, 'package.json');
-
-if (!module.exports.version) {
-  module.exports.version = JSON.parse(
-    fs.readFileSync(pack_file, 'utf8')).version;
-}
